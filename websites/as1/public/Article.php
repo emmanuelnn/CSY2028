e9376dcd295a58d42a79eb236ea63c150c3cd985
emmanuel <emmanuel.nwokoro20@my.northampton.ac.uk>
2021-11-21 19:33:51 +0000

    first commit

--- websites/as1/public/Article.php
+++ websites/as1/public/Article.php
@@ -39,6 +39,14 @@ $row = $stmt->fetch();
 
 <?php
 }
+$server = 'mysql';
+$username = 'student';
+$password = 'student';
+
+
+$schema = 'assignment1';
+
+$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password);
 
 
 if (isset($_POST['submit'])) {
